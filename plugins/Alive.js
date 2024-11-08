ba4aa974c7d3ac4e87d7e10fda5445852b558242
THUNDERluzer <95018914+THUNDERluzer@users.noreply.github.com>
2024-11-08 15:52:46 +0530

    Update Alive.js

--- plugins/Alive.js
+++ plugins/Alive.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   // Sound
   let name = m.pushName || conn.getName(m.sender);
-  let img = 'https://i.imgur.com/s0SqG3g.jpeg';
+  let img = 'https://telegra.ph/file/7721cf222f9790f3fcd1b.jpg';
   let con = {
     key: {
       fromMe: false,
@@ -17,14 +17,14 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
   };
 
   let messageContent = {
-    text: '𝗨𝗟𝗧𝗥𝗔-𝗠𝗗 𝗜𝗦 𝗥𝗨𝗡𝗡𝗜𝗡𝗚', // Text content in case a message body is needed
+    text: 'Panda-𝗠𝗗 𝗜𝗦 𝗥𝗨𝗡𝗡𝗜𝗡𝗚', // Text content in case a message body is needed
     contextInfo: {
       mentionedJid: [m.sender],
       externalAdReply: {
-        title: '© 𝙶𝚕𝚘𝚋𝚊𝚕𝚃𝚎𝚌𝚑𝙸𝚗𝚏𝚘',
-        body: '© GlobalTechInfo',
+        title: '© Hashan',
+        body: '© Hashan',
         thumbnailUrl: img,
-        sourceUrl: 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07',
+        sourceUrl: 'https://wa.me/qr/P6WADRJOKAFUK1',
         mediaType: 1,
         renderLargerThumbnail: true,
       },
